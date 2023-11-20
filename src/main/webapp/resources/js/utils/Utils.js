2f7b1702e11f49c07266b0e9f4390c6ddec62f37
Vladislav Andreev <andreevvlad443@gmail.com>
2023-11-20 22:03:17 +0300

    feat: main page

---
+++ src/main/webapp/resources/js/utils/Utils.js
@@ -0,0 +1,9 @@
+// applyes an error message to error block
+function applyErrorMessage(message) {
+    $('#error-message').text(message);
+}
+
+// clears error block
+function clearErrors() {
+    $('#error-message').text('');
+}
